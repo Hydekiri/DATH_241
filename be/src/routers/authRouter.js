2b6ba153899b0533525e7b1080bcaa4d965c6f77
DANG VAN TAN <161315905+tank22BKU@users.noreply.github.com>
2024-12-26 21:31:11 +0700

    Encoded password

--- be/src/routers/authRouter.js
+++ be/src/routers/authRouter.js
@@ -2,4 +2,5 @@ module.exports = (router) => {
     const authController = require("../controllers/authController");
 
     router.post("/auth/login", authController.signIn);
+    router.post("/auth/register", authController.register);
 };
