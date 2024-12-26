1438cd3eaa9fbe757102ad827fe021c85514c089
caocaobay <tranuyqhc1@gmail.com>
2024-12-26 23:32:16 +0700

    update student buy

--- fe/scripts/student-buy.js
+++ fe/scripts/student-buy.js
@@ -13,3 +13,16 @@ plusButton.addEventListener('click', function() {
   let currentValue = parseInt(numPageInput.value);
   numPageInput.value = currentValue + 1;
 });
+
+
+const payButton = document.querySelector('.button-pay'),
+overlay = document.querySelector('.overlay'),
+returnButton = document.querySelector('.return');
+
+payButton.addEventListener('click', () => {
+  overlay.classList.remove('hidden');
+});
+
+returnButton.addEventListener('click', () => {
+  overlay.classList.add('hidden');
+})
