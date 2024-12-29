d740f98f123466a417f8f5dd522319cd6cddfd9f
DANG VAN TAN <161315905+tank22BKU@users.noreply.github.com>
2024-12-29 19:12:48 +0700

    adding inittial logout function ( have yet add html,css)

--- fe/scripts/general/admin-navbar.js
+++ fe/scripts/general/admin-navbar.js
@@ -1,3 +1,20 @@
+function LogOutSPSO() {
+    const avatar = document.querySelector('.admin-navbar-avatar .avatar');
+
+    if (avatar) {
+        avatar.addEventListener("click", () => {
+            alert("Logging out...");
+
+            document.cookie = "token=";
+            document.cookie = "id=";
+
+            window.location.href = "./welcome.html";
+        });
+    } else {
+        console.error("Avatar element not found. Check your HTML structure.");
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   fetch('http://127.0.0.1:5500/fe/scripts/general/admin-navbar.html')
       .then(response => response.text())
--- fe/scripts/general/student-navbar.js
+++ fe/scripts/general/student-navbar.js
@@ -1,3 +1,20 @@
+function LogOutSTD() {
+    const avatar = document.querySelector('.student-navbar-avatar .avatar');
+
+    if (avatar) {
+        avatar.addEventListener("click", () => {
+            alert("Logging out...");
+
+            document.cookie = "token=";
+            document.cookie = "id=";
+
+            window.location.href = "./welcome.html";
+        });
+    } else {
+        console.error("Avatar element not found. Check your HTML structure.");
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   fetch('http://127.0.0.1:5500/fe/scripts/general/student-navbar.html')
       .then(response => response.text())
