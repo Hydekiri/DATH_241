a102f028b6206c79e979f2310556ee201601471c
Nguyễn Minh Tú <tu.nguyenhydekiri@hcmut.edu.vn>
2024-12-30 21:12:08 +0700

    Update ver 15.3

--- be/src/models/printQueueModel.js
+++ be/src/models/printQueueModel.js
@@ -63,65 +63,65 @@ const queuePrinterModel = {
 
             // 5. Thêm bản ghi vào `Queue`
             await pool.query(
-                `INSERT INTO Queue (
-                userID, printer_ID, config_ID, document_name, queue_position, status, numPages, print_start
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-                [userId, printerId, configId, document.name, queuePosition, status, numPages, printStart]
-            );
-
-        } catch (error) {
-            console.error("Error for add to queue", error);
-            throw error;
-        }
-    },
-    processPrintQueue: async (printer_ID) => {
-        try {
-            // 1. Lấy tài liệu đang in cho máy in
-            const [currentJob] = await pool.query(
-                `SELECT * FROM Queue 
-                     WHERE printer_ID = ? AND status = '0' 
-                     ORDER BY queue_position LIMIT 1`,
-                [printer_ID]
-            );
-
-            if (currentJob) {
-                console.log(`Printing document on Printer ${printer_ID}: ${currentJob.document_name}`);
-
-                // 2. Giả lập thời gian in (10 giây mỗi trang)
-                currentJob.print_start = new Date();
-                const printTime = currentJob.numPages * 10000; // numPages * 10 giây
-                await sleep(printTime);
-                console.log(`Completed printing on Printer ${printer_ID}: ${currentJob.document_name}`);
-                currentJob.print_end = new Date();
-
-                // 3. Xóa tài liệu đã in
-                await pool.query(
-                    `DELETE FROM Queue WHERE queue_ID = ?`,
-                    [currentJob.queue_ID]
+                    `INSERT INTO Queue (
+                    userID, printer_ID, config_ID, document_name, queue_position, status, numPages, print_start
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+                    [userId, printerId, configId, document.name, queuePosition, status, numPages, printStart]
                 );
 
-                // 4. Dịch chuyển hàng đợi và cập nhật trạng thái tài liệu tiếp theo
-                await pool.query(
-                    `UPDATE Queue 
-                         SET queue_position = queue_position - 1 
-                         WHERE printer_ID = ? AND queue_position > ?`,
-                    [printer_ID, currentJob.queue_position]
-                );
-
-                await pool.query(
-                    `UPDATE Queue 
-                         SET status = '0', print_start = NOW() 
-                         WHERE printer_ID = ? AND queue_position = 1 AND status = '1'`,
+            } catch (error) {
+                console.error("Error for add to queue", error);
+                throw error;
+            }
+        },
+        processPrintQueue: async (printer_ID) => {
+            try {
+                // 1. Lấy tài liệu đang in cho máy in
+                const [currentJob] = await pool.query(
+                    `SELECT * FROM Queue 
+                        WHERE printer_ID = ? AND status = '0' 
+                        ORDER BY queue_position LIMIT 1`,
                     [printer_ID]
                 );
-            } else {
-                console.log(`Queue for Printer ${printer_ID} is empty. Waiting for new jobs...`);
+
+                if (currentJob) {
+                    console.log(`Printing document on Printer ${printer_ID}: ${currentJob.document_name}`);
+
+                    // 2. Giả lập thời gian in (10 giây mỗi trang)
+                    currentJob.print_start = new Date();
+                    const printTime = currentJob.numPages * 10000; // numPages * 10 giây
+                    await sleep(printTime);
+                    console.log(`Completed printing on Printer ${printer_ID}: ${currentJob.document_name}`);
+                    currentJob.print_end = new Date();
+
+                    // 3. Xóa tài liệu đã in
+                    await pool.query(
+                        `DELETE FROM Queue WHERE queue_ID = ?`,
+                        [currentJob.queue_ID]
+                    );
+
+                    // 4. Dịch chuyển hàng đợi và cập nhật trạng thái tài liệu tiếp theo
+                    await pool.query(
+                        `UPDATE Queue 
+                            SET queue_position = queue_position - 1 
+                            WHERE printer_ID = ? AND queue_position > ?`,
+                        [printer_ID, currentJob.queue_position]
+                    );
+
+                    await pool.query(
+                        `UPDATE Queue 
+                            SET status = '0', print_start = NOW() 
+                            WHERE printer_ID = ? AND queue_position = 1 AND status = '1'`,
+                        [printer_ID]
+                    );
+                } else {
+                    console.log(`Queue for Printer ${printer_ID} is empty. Waiting for new jobs...`);
+                    await sleep(5000);
+                }
+            } catch (err) {
+                console.error(`Error processing queue for Printer ${printer_ID}:`, err.message);
                 await sleep(5000);
             }
-        } catch (err) {
-            console.error(`Error processing queue for Printer ${printer_ID}:`, err.message);
-            await sleep(5000);
         }
-    }
 };
 module.exports = queuePrinterModel;
