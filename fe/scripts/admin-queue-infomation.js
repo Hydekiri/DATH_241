7e60d070e246953ca4b392e52c7acc5ab65da02b
caocaobay <tranuyqhc1@gmail.com>
2024-12-30 20:19:01 +0700

    update queue

--- fe/scripts/admin-queue-infomation.js
+++ fe/scripts/admin-queue-infomation.js
@@ -1,6 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const printer_ID = urlParams.get('printer_ID');
 let printerStatus;
+let printerPage;
+let branchName1, model1, description1, status1, weight1, printer_type1, queue1, prints_in_day1, pages_printed1, color_print1, printer_size1, paper_size1, resolution1, ink_type1, location1;
 
 console.log(printer_ID); // Hiển thị printer_ID lên console
 
@@ -25,6 +27,25 @@ const fetchPrinterInfo = async () => {
 // Hàm render thông tin chung của máy in
 const renderPrinterInfo = (printer) => {
     printerStatus = printer.status;
+    printerPage = printer.pages_printed;
+
+    branchName1 = printer.branchName;
+    model1 = printer.model;
+    description1 = printer.description;
+    status1 = printer.status;
+    weight1 = printer.weight;
+    printer_type1 = printer.printer_type;
+    queue1 = printer.model;
+    prints_in_day1 = printer.prints_in_day;
+    pages_printed1 = printer.pages_printed;
+    color_print1 = printer.color_print;
+    printer_size1 = printer.printer_size;
+    paper_size1 = printer.paper_size;
+    resolution1 = printer.resolution;
+    ink_type1 = printer.ink_type;
+    location1 = printer.location;
+
+    console.log(location1);
 
     document.querySelector(".IDPrinter").innerHTML = `<span>ID:</span> ${printer.Printer_ID}`;
     document.querySelector(".model").innerHTML = `<span>Model:</span> ${printer.model}`;
@@ -68,7 +89,7 @@ async function fetchPrintConfig() {
 }
 
 function renderPrintConfig(printconfigs) {
-    if (printerStatus !== 'enable')
+    if (printerStatus != "enable")
     {
         document.querySelector('.printers-display').innerHTML += '<h1 style="text-align: center">Máy in hiện không khả dụng!</h1>';
         return;
@@ -76,22 +97,35 @@ function renderPrintConfig(printconfigs) {
 
     const printconfigDisplay = document.querySelector('.printconfig-display');
     let html = '';
+    let index = 0;
 
     printconfigs.forEach((printconfig) => {
         if (printconfig.status !== 'unCompleted') return;
 
+        index++;
+        //Tinh toan lai dinh dang thoi gian
         const datetimeStr = printconfig.printStart;
         const [date, timeWithTimezone] = datetimeStr.split('T');
         const time = timeWithTimezone.replace('.000Z', '');
+
+
+        //Tinh so luong giay can in
+        let paperToPrint = printconfig.printSide === 'one-side' ? printconfig.numPages : Math.ceil(printconfig.numPages / 2);
+
+        if (printconfig.paperSize === 'A3') paperToPrint *= 2;
+
+        paperToPrint *= printconfig.numCopies;
+
+        console.log(paperToPrint);
 
         html += `
             <tr class="success">
                 <td>${printconfig.user.name}<br>${printconfig.user.user_ID}</td>
                 <td>${date} <br>${time}</br></td>
-                <td>${printconfig.config_ID}</td>
-                <td>${printconfig.numPages} <br>${printconfig.paperSize}</br></td>
+                <td>${index}</td>
+                <td>${paperToPrint} <br>${printconfig.paperSize}</br></td>
                 <td>${printconfig.documents.map(doc => doc.name).join('<br>')}<br>Đã kiểm duyệt</td>
-                <td class="print-action"><button class="print-button" onclick="handlePrintButton(${printconfig.config_ID})">In</button></td>
+                <td class="print-action"><button class="print-button" onclick="handlePrintButton(${printconfig.config_ID}, '${paperToPrint}')">In</button></td>
             </tr>
         `;
     });
@@ -100,9 +134,78 @@ function renderPrintConfig(printconfigs) {
     if (html === '') document.querySelector('.printers-display').innerHTML += '<h1 style="text-align: center">Không có tài liệu đang đợi in</h1>';
 }
 
-async function handlePrintButton(config_ID) {
+async function updatePrinter(printerPage) {
+    printerPage = Number(printerPage);
+
+    try {
+        console.log(printerPage);
+        console.log({
+            branchName: branchName1,
+            model: model1,
+            description: description1,
+            status: status1,
+            weight: weight1,
+            printer_type: printer_type1, 
+            queue: queue1,              
+            prints_in_day: prints_in_day1 + 1, 
+            pages_printed: printerPage, 
+            color_print: color_print1,   
+            printer_size: printer_size1,  
+            paper_size: paper_size1,     
+            resolution: resolution1,      
+            ink_type: ink_type1, 
+            location: location
+        });
+
+        const dataToUpdate = {
+            branchName: branchName1,
+            model: model1,
+            description: description1,
+            status: status1,
+            weight: weight1,
+            printer_type: printer_type1, 
+            queue: queue1,              
+            prints_in_day: prints_in_day1 + 1, 
+            pages_printed: printerPage, 
+            color_print: color_print1,   
+            printer_size: printer_size1,  
+            paper_size: paper_size1,     
+            resolution: resolution1,      
+            ink_type: ink_type1, 
+            location: location1
+        };
+
+        const response = await fetch(`http://localhost:3000/api/d1/printers/${printer_ID}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dataToUpdate)
+        });
+        if (!response.ok) {
+            throw new Error("Không thể cập nhật máy in!");
+        }
+
+        alert('cap nhat may in thanh cong');
+    }
+    catch {
+        console.error("Lỗi khi cập nhật máy in:", error);
+        alert("Có lỗi xảy ra khi cập nhật máy in.");
+    }
+}
+
+async function handlePrintButton(config_ID, paperNeeded) {
     const confirmPrint = window.confirm("Xác nhận in tài liệu này?");
     if (!confirmPrint) return;
+
+    paperNeeded = Number(paperNeeded);
+
+    console.log(paperNeeded);
+
+    if (paperNeeded > printerPage) {
+        alert('Máy in không đủ giấy, hãy nạp thêm!');
+        return;
+    }
 
     try {
         const response = await fetch(`http://localhost:3000/api/d1/printconfigs/${config_ID}`, {
@@ -117,6 +220,8 @@ async function handlePrintButton(config_ID) {
             throw new Error("Không thể cập nhật trạng thái cấu hình in!");
         }
 
+        await updatePrinter(`${printerPage - paperNeeded}`);
+
         alert("In thành công và trạng thái đã được cập nhật.");
         fetchPrintConfig();
     } catch (error) {
@@ -126,9 +231,9 @@ async function handlePrintButton(config_ID) {
 }
 
 // Gọi hàm fetchPrinterInfo khi trang được tải
-window.onload = () => {
-    fetchPrintConfig();
-    fetchPrinterInfo();
+window.onload = async () => {
+    await fetchPrinterInfo();
+    fetchPrintConfig();   
 }
 
 // Tạo sự kiện "Trở lại"
