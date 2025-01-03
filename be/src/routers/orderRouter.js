0241451d84d90a64137b4a94328392afa784ddf7
SuperCulier <doquy175@gmail.com>
2025-01-04 03:00:15 +0700

    Cap nhật số trang in sau khi thanh toán thành công

--- be/src/routers/orderRouter.js
+++ be/src/routers/orderRouter.js
@@ -17,18 +17,17 @@ module.exports = (router) => {
     const orderController = require("../controllers/orderController");
     // API: Mua số trang in
     router.post("/buypages", orderController.buyPages);
-    /*router.post("/buypages", (req, res, next) => {
-        console.log("POST /buypages called");
-        next(); // Chuyển tiếp request đến controller
-    }, orderController.buyPages);*/
+
+   //API update số trang in
+   router.put("/updatepages", orderController.updatePages);
 
    // API: Lấy đơn hàng theo ID
-   router.get("/checkorderid", orderController.getOrderById);
+   //router.get("/checkorderid", orderController.getOrderById);
 
    // API: Lấy tất cả đơn hàng của một người dùng
-   router.get("/checkorderuser", orderController.getAllOrdersByUser);
+   //router.get("/checkorderuser", orderController.getAllOrdersByUser);
+
    //API tạo link thanh toán QR'
    router.post("/create-payment-link", orderController.CreatePaymentLink);
 };
-
 
